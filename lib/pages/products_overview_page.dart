f83661fbe0d3bdc0aeb58cd21cee4fc9c93bd673
absjuniordev <abs.junnior@hotmail.com>
2022-11-17 19:35:30 -0300

    Componente Item de Produto

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
@@ -13,18 +14,16 @@ class ProductsOverviewPage extends StatelessWidget {
         centerTitle: true,
         title: const Text("Minha Loja"),
       ),
-      body: Padding(
+      body: GridView.builder(
         padding: const EdgeInsets.all(10),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        itemCount: loadedProducts.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
       ),
     );
   }
