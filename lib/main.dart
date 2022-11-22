e8d13b46af4961592724de436caf08d006dc9021
absjuniordev <abs.junnior@hotmail.com>
2022-11-21 23:01:22 -0300

    Sem alterãcoes do Inherited Widget na Prática

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop/pages/counter_page.dart';
-
+import 'package:shop/pages/product_detail_pages.dart';
 import 'package:shop/pages/products_overview_page.dart';
-import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
 
 void main() {
@@ -15,22 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: Colors.purple,
-            ).copyWith(
-              secondary: Colors.deepOrange,
-            ),
-            fontFamily: 'Lato'),
-        home: ProductsOverviewPage(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => const CounterPage(),
-        },
-        debugShowCheckedModeBanner: false,
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            primarySwatch: Colors.purple,
+          ).copyWith(
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato'),
+      home: ProductsOverviewPage(),
+      routes: {
+        AppRoutes.PRODUCT_DETAIL: (ctx) => const ProductDatailPage(),
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
