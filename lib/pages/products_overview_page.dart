cc2b29c4eec35b67b7085a5cdd34d86cd4d972d3
absjuniordev <abs.junnior@hotmail.com>
2022-11-24 21:15:04 -0300

    Filtrar Favoritos #01

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 
 import '../components/product_grid.dart';
+
+enum FilterOpstions {
+  Favorite,
+  All,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: const Text("Minha Loja"),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOpstions.Favorite,
+                child: Text("Somente Favoritos"),
+              ),
+              const PopupMenuItem(
+                value: FilterOpstions.All,
+                child: Text("Todos"),
+              ),
+            ],
+            onSelected: (FilterOpstions selectedValue) {
+              if (selectedValue == FilterOpstions.Favorite) {
+                provider.showFavoriteOnly();
+              } else {
+                provider.showAll();
+              }
+            },
+          )
+        ],
       ),
       body: const ProductGrid(),
     );
