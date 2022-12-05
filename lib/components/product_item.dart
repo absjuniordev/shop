0dee6b6e577f394a160fc7ebee2d01f881e7d672
absjuniordev <abs.junnior@hotmail.com>
2022-12-05 20:32:58 -0300

    Filtrar Favoritos #03

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -33,7 +33,7 @@ class ProductItem extends StatelessWidget {
             ),
           ),
           title: Text(
-            product.title,
+            product.name,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
