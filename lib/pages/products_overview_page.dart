6bf2289d921b347073a92ceaa764a5c29722ff70
absjuniordev <abs.junnior@hotmail.com>
2022-12-05 22:15:38 -0300

    Componente Carrinho #01

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/badge.dart';
+import 'package:shop/components/product_grid.dart';
 import 'package:shop/models/cart.dart';
-import 'package:shop/models/product_list.dart';
 import 'package:shop/utils/app_routes.dart';
 
-import '../components/product_grid.dart';
-
-enum FilterOpstions {
-  Favorite,
-  All,
+enum FilterOptions {
+  favorite,
+  all,
 }
 
 class ProductsOverviewPage extends StatefulWidget {
-  const ProductsOverviewPage({super.key});
+  const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   State<ProductsOverviewPage> createState() => _ProductsOverviewPageState();
@@ -24,33 +22,30 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Minha Loja"),
+        title: const Text('Minha Loja'),
         actions: [
           PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
             itemBuilder: (_) => [
               const PopupMenuItem(
-                value: FilterOpstions.Favorite,
-                child: Text("Somente Favoritos"),
+                value: FilterOptions.favorite,
+                child: Text('Somente Favoritos'),
               ),
               const PopupMenuItem(
-                value: FilterOpstions.All,
-                child: Text("Todos"),
+                value: FilterOptions.all,
+                child: Text('Todos'),
               ),
             ],
-            onSelected: (FilterOpstions selectedValue) {
-              setState(
-                () {
-                  if (selectedValue == FilterOpstions.Favorite) {
-                    _showFavoriteOnly = true;
-                  } else {
-                    _showFavoriteOnly = false;
-                  }
-                },
-              );
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.favorite) {
+                  _showFavoriteOnly = true;
+                } else {
+                  _showFavoriteOnly = false;
+                }
+              });
             },
           ),
           Consumer<Cart>(
