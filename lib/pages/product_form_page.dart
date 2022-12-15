5bdda62d6206121777fa8621b5e45f6da2ecc5e9
absjuniordev <abs.junnior@hotmail.com>
2022-12-14 21:54:32 -0300

    Controlando o Foco

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -10,6 +10,8 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   @override
   Widget build(BuildContext context) {
+    final _priceFocus = FocusNode();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Formulario de Produto'),
@@ -22,6 +24,17 @@ class _ProductFormPageState extends State<ProductFormPage> {
             TextFormField(
               decoration: const InputDecoration(labelText: 'Nome'),
               textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_) {
+                FocusScope.of(context).requestFocus(_priceFocus);
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(labelText: 'Preço'),
+              textInputAction: TextInputAction.next,
+              focusNode: _priceFocus,
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
             )
           ],
         )),
