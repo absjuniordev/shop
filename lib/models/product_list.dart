255aac79b9980cf644ab3f34517b52275cbe3ff7
absjuniordev <abs.junnior@hotmail.com>
2022-12-26 18:41:50 -0300

    Primeiro POS no FIREBASE

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,12 +1,14 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _items = dummyProducts;
-
+  final _baseUrl = 'https://shop-jr-df699-default-rtdb.firebaseio.com';
   List<Product> get items => [..._items];
   List<Product> get favotieItems =>
       _items.where((prod) => prod.isFavorite).toList();
@@ -34,6 +36,19 @@ class ProductList with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    http.post(
+      Uri.parse('$_baseUrl/product.json'),
+      body: jsonEncode(
+        {
+          "name": product.name,
+          "description": product.description,
+          "price": product.price,
+          "imageUrl": product.imageUrl,
+          "isFavorite": product.isFavorite,
+        },
+      ),
+    );
+
     _items.add(product);
     notifyListeners();
   }
