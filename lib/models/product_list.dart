e5688cfd64afde46f8e88170bf7fafdc55d7b4a0
absjuniordev <abs.junnior@hotmail.com>
2023-01-03 21:37:26 -0300

    Atualizando o Produto

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _url = 'https://shop-jr-df699-default-rtdb.firebaseio.com/product.json';
+  final _baseUrl = 'https://shop-jr-df699-default-rtdb.firebaseio.com/product';
   final List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -19,7 +19,9 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     _items.clear();
-    final response = await http.get(Uri.parse(_url));
+    final response = await http.get(
+      Uri.parse('$_baseUrl.json'),
+    );
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach((productId, productData) {
@@ -54,7 +56,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$_baseUrl.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -79,9 +81,21 @@ class ProductList with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> updateProduct(Product product) {
+  Future<void> updateProduct(Product product) async {
     int index = _items.indexWhere((p) => p.id == product.id);
+
     if (index >= 0) {
+      await http.patch(
+        Uri.parse('$_baseUrl/.${product.id}.json'),
+        body: jsonEncode(
+          {
+            "name": product.name,
+            "description": product.description,
+            "price": product.price,
+            "imageUrl": product.imageUrl,
+          },
+        ),
+      );
       _items[index] = product;
       notifyListeners();
     }
