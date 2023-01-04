280741f6f6e0bf69283b973006f3f6d10c3e8335
absjuniordev <abs.junnior@hotmail.com>
2023-01-04 19:33:20 -0300

    Armazenando favoritos

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import '../utils/constants.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,9 +21,26 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
-
-  void toggleFavorite() {
+  void _toggleFavorite() {
     isFavorite = !isFavorite;
     notifyListeners();
+  }
+
+  Future<void> toggleFavorite() async {
+    try {
+      _toggleFavorite();
+      final response = await http.patch(
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/$id.json'),
+        body: jsonEncode(
+          {'isFavorite': isFavorite},
+        ),
+      );
+
+      if (response.statusCode >= 400) {
+        _toggleFavorite();
+      }
+    } catch (_) {
+      _toggleFavorite();
+    }
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const PRODUCT_BASE_URL =
+      'https://shop-jr-df699-default-rtdb.firebaseio.com/product';
+}
